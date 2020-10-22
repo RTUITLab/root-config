f42270d96f7a7f34c4812eb72c190c59163d7fe7
IvLaptev <ivlaptev13@ya.ru>
2020-10-22 10:49:31 +0300

    Added routing

---
+++ src/activity-functions.js
@@ -0,0 +1,13 @@
+export function prefix(location, ...prefixes) {
+  return prefixes.some(
+    (prefix) => location.href.indexOf(`${location.origin}/${prefix}`) !== -1
+  );
+}
+
+export function itLabFront() {
+  return true;
+}
+
+export function projectsFront() {
+  return prefix(location, "projects");
+}
--- src/rtuitlab-root-config.js
+++ src/rtuitlab-root-config.js
@@ -1,4 +1,6 @@
 import { registerApplication, start } from "single-spa";
+import * as isActive from "./activity-functions";
+import frontends from "./config.json";
 
 // registerApplication({
 //   name: "@single-spa/welcome",
@@ -10,16 +12,19 @@ import { registerApplication, start } from "single-spa";
 // });
 
 registerApplication({
-  name: "@rtuitlab/itlab-front",
-  app: () => System.import("@rtuitlab/itlab-front"),
-  activeWhen: ["/"],
+  name: "itlab-front",
+  app: () => System.import(frontends.itlab),
+  activeWhen: isActive.itLabFront,
 });
 
 registerApplication({
-  name: "@rtuitlab/projects",
-  app: () => System.import("@rtuitlab/projects"),
-  activeWhen: ["/projects"],
-  customProps: { projectsAPIUrl: "http://localhost:5506/api/projects" },
+  name: "projects",
+  app: () => System.import(frontends.projects),
+  activeWhen: isActive.projectsFront,
+  customProps: {
+    projectsAPIUrl: "http://localhost:5506/api/projects",
+    domElement: document.getElementById("projects-page"),
+  },
 });
 
 start({
